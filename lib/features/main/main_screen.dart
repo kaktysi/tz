60a5ff1ecf8a343e0030c32c6d70999af439ee5e
Gemek1 <dzmitry.mikulich@innowise.com>
2025-08-13 18:57:46 +0300

    fix

--- lib/features/main/main_screen.dart
+++ lib/features/main/main_screen.dart
@@ -2,23 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tz/features/features.dart';
 import 'package:tz/theme/app_colors.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(primarySwatch: Colors.deepPurple, useMaterial3: true),
-      home: MainScreen(),
-    );
-  }
-}
-
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
